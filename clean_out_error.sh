15ad5c40875018bd76c9fa70d4ff987c5cbb86cd
jerome servonnat <jservon@ciclad-ng.private.ipsl.fr>
2019-01-23 10:29:54 +0100

    Remove climaf_mcdo_* when using clean_out_error.sh

--- clean_out_error.sh
+++ clean_out_error.sh
@@ -3,7 +3,7 @@
 
 
 echo 'List of files for the cleanup = '
-ls */climaf.log */last.out */*/climaf.log */*/last.out */*.e* */*.o* */*/*.e* */*/*.o* */*/job.in */job.in
+ls */climaf.log */last.out */*/climaf.log */*/last.out */*.e* */*.o* */*/*.e* */*/*.o* */*/job.in */job.in */climaf_mcdo_* */*/climaf_mcdo_*
 echo 'Do you want to remove those files? (y/n - o/n - yes/no - oui/non) => '
 echo -n " Your answer : "
 read reponse
@@ -11,6 +11,7 @@ case ${reponse} in
 y|o|yes|oui)
 echo 'Erasing the files...'
 rm -f */climaf.log */last.out */*/climaf.log */*/last.out */*.e* */*.o* */*/*.e* */*/*.o* */*/job.in */job.in
+rm -rf */climaf_mcdo_* */*/climaf_mcdo_*
 echo 'Done'
 ;;
 n|no|non)
