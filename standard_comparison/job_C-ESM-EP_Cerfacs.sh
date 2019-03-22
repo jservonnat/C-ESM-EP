2b118c0f8e891cb0a3115055d5f16b9d52621361
jerome servonnat <jservon@ciclad-ng.private.ipsl.fr>
2019-03-22 17:31:32 +0100

    with job_C-ESM-EP_Cerfacs.sh

---
+++ standard_comparison/job_C-ESM-EP_Cerfacs.sh
@@ -0,0 +1,104 @@
+#!/bin/bash -l
+#SBATCH --partition visu
+# Nom du job
+#SBATCH --job-name CESMEP
+# Temps limite du job
+#SBATCH --time=03:00:00
+#SBATCH --output=cesmep.o
+#SBATCH --error=cesmep.e
+# Nombre de noeuds et de processus  
+#SBATCH --nodes=1 --ntasks-per-node=36
+set -x
+# -------------------------------------------------------- >
+# --
+# -- Script to run a CliMAF atlas on Ciclad:
+# --   - sets up the environment
+# --   - specify the parameter file and the season
+# --   - automatically sets up the CliMAF cache
+# --   - and run the atlas
+# --
+# --
+# --     Author: Jerome Servonnat
+# --     Contact: jerome.servonnat__at__lsce.ipsl.fr
+# --
+# --
+# -------------------------------------------------------- >
+date
+
+
+# -- On doit pouvoir le soumettre en batch, ou le soumettre en interactif dans le repertoire de la composante
+# -> # -- On doit pouvoir le soumettre en batch, ou le soumettre en interactif dans le repertoire de la composante
+
+# -> Separer le cas batch et le cas interactif : identifier les deux
+
+# -- Specify the atlas script
+# -------------------------------------------------------- >
+atlas_file='main_C-ESM-EP.py'
+env_script='setenv_C-ESM-EP.sh'
+
+# -- Cas interactif depuis le repertoire de la comparaison
+if [[ $1 != '' ]]; then
+
+  component=${1%/}
+  comparison=$(basename $PWD)
+  #comparison=$(basename $(dirname $0) | sed 's=/==g')
+  env=../${env_script}
+  main=../${atlas_file}
+  datasets_setup_file=datasets_setup.py
+  # -- Name of the parameter file
+  param_file=${component}/params_${component}.py
+
+else
+
+  # -- comparison, component et WD sont les variables passees avec qsub -v
+  echo '$comparison'
+  echo $comparison
+  echo '$component'
+  echo $component
+  echo '$WD'
+  echo $WD
+  env=../../${env_script}
+  main=../../${atlas_file}
+  datasets_setup_file=../datasets_setup.py
+  if [[ -n ${WD} ]]; then
+     cd $WD
+  fi
+  component=${component%/}
+  # -- Name of the parameter file
+  param_file=params_${component}.py
+
+fi
+
+
+# -- Setup the environment...
+# -------------------------------------------------------- >
+source ${env}
+
+# -- Provide a season
+# -------------------------------------------------------- >
+season='ANM'
+
+# -- Set CliMAF cache (automatically)
+# -------------------------------------------------------- >
+if [[ -d "/ccc" && ! -d "/data" ]]; then
+export CLIMAF_CACHE=${SCRATCHDIR}/climafcache_${component}
+fi
+
+if [[ -d "/data" ]]; then
+export CLIMAF_CACHE=/prodigfs/ipslfs/dods/${USER}/climafcache_${component}
+fi
+
+if [[ -d "/cnrm" ]]; then
+    [ -z $CLIMAF_CACHE ] &&  echo "CLIMAF_CACHE should be set by launch job" && exit 1
+    export CLIMAF_CACHE
+fi
+
+# -- Run the atlas...
+# -------------------------------------------------------- >
+echo "Running ${atlas_file} for season ${season} with parameter file ${param_file}"
+echo "Using CliMAF cache = ${CLIMAF_CACHE}"
+echo python ${main} -p ${param_file} --season ${season} --datasets_setup ${datasets_setup_file} --comparison ${comparison}
+
+python ${main} -p ${param_file} --season ${season} --datasets_setup ${datasets_setup_file} --comparison ${comparison}
+
+
