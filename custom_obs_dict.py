c960b49dd687c21a57cb887752a7f01639956152
jerome servonnat <jservon@ciclad-ng.private.ipsl.fr>
2019-11-27 17:11:46 +0100

    Forgot to add custom_obs_dict

---
+++ custom_obs_dict.py
@@ -0,0 +1,8 @@
+custom_obs_dict = {
+        #'NO3':dict(project='ref_climatos', product='WOA09', frequency='yr'),
+        #'Si' :dict(project='ref_climatos', product='WOA09', frequency='yr'),
+        #'O2' :dict(project='ref_climatos', product='WOA09', frequency='yr'),
+        #'PO4':dict(project='ref_climatos', product='WOA09', frequency='yr'),
+        #'alk':dict(project='MY_REFs', frequency='annual', product='GLODAPv2.1', resolution='r360x180xl31'),
+        }
+
