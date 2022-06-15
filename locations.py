ea495a480b0703669d3792d6c7cd8283e80d2029
Stéphane Sénési <senesi@posteo.net>
2022-06-15 15:24:32 +0200

    Module locations handles spirit and uses env.site_settings (except at TGCC)

--- locations.py
+++ locations.py
@@ -9,6 +9,16 @@ from __future__ import unicode_literals, print_function, absolute_import, divisi
 
 import getpass
 import os
+
+# Identify on which system we are running, generally using a CliMAF module
+if not os.path.exists('/ccc') or os.path.exists('/data'):
+    from env.site_settings import atCNRM, onCiclad, onSpirit, atTGCC, atIDRIS, atIPSL, onSpip, atCerfacs
+else:
+    # On TGCC, we wish to avoid having to use CliMAF in this utility script, because
+    # CliMAF is available through a docker container, and we would like to avoid using
+    # this slightly heavy procedure when executing this script
+    from tgcc_settings import atCNRM, onCiclad, onSpirit, atTGCC, atIDRIS, atIPSL, onSpip, atCerfacs
+
 username = getpass.getuser()
 
 # -- path_to_cesmep_output_rootdir is the location of the root output directory
@@ -24,7 +34,7 @@ username = getpass.getuser()
 # path_to_cesmep_output_rootdir_on_web_server = None
 
 
-if os.path.exists('/cnrm'):  
+if atCNRM:
     # Climaf Cache location - used for launching batch jobs in run_C-ESM-EP.py
     # This could be a location shared among users.
     # This can also be a location visible only from compute cluster (aneto)
@@ -44,7 +54,7 @@ if os.path.exists('/cnrm'):
     path_to_cesmep_output_rootdir_on_web_server = None
     
 # -- Ciclad
-if os.path.exists('/data') and os.path.exists('/thredds/ipsl'):
+if onCiclad:
     # -- path_to_cesmep_output_rootdir is the location of the root output directory
     # -- where we store all the C-ESM-EP comparisons
     #path_to_cesmep_output_rootdir = '/thredds/ipsl/'+username
@@ -56,9 +66,24 @@ if os.path.exists('/data') and os.path.exists('/thredds/ipsl'):
     #    than the path where you actually stored your data (ex: path to thredds )
     # -- Can be equal to store_atlas_results_dir, but at TGCC
     path_to_cesmep_output_rootdir_on_web_server = None
+    #
+    climaf_cache = '/scratchu/' + username + '/atlas_explorer'
+
+# -- Spirit, without web
+if onSpirit:
+    # -- path_to_cesmep_output_rootdir is the location of the root output directory
+    # -- where we store all the C-ESM-EP comparisons
+    path_to_cesmep_output_rootdir = '/scratchu/'+username
+    # --
+    # -- Path that follows root_url to access path_to_cesmep_output_rootdir from a web page
+    root_url_to_cesmep_outputs = 'file://'+path_to_cesmep_output_rootdir
+    path_to_cesmep_output_rootdir_on_web_server = None
+    #
+    climaf_cache = '/scratchu/' + username + '/atlas_explorer'
+
 
 # -- TGCC
-if os.path.exists('/ccc') and not os.path.exists('/thredds/ipsl'):
+if atTGCC:
     CWD = os.getcwd()
     if '/drf/' in CWD:
         wspace = 'drf'
@@ -73,47 +98,48 @@ if os.path.exists('/ccc') and not os.path.exists('/thredds/ipsl'):
     path_to_cesmep_output_rootdir_on_web_server = str.replace(str.replace(path_to_cesmep_output_rootdir,
                                                                            '/scratch/', '/work/'),
                                                               '/'+wspace+'/', '/thredds/')
+    climaf_cache=os.getenv('SCRATCHDIR') + '/cache_atlas_explorer'
+
 
 # At Cerfacs
-if os.path.exists('/scratch/globc'):
-    # Climaf Cache location - used for launching batch jobs in run_C-ESM-EP.py
-    # This could be a location shared among users.
-    # This can also be a location visible only from compute cluster (aneto)
-    climaf_cache = '/scratch/globc/' + username + '/C-ESM-EP/CESMEP_climaf_cache'
+if atCerfacs:
+    # On scylla
+    if os.path.exists('/data/scratch/globc'):
+        # Climaf Cache location - used for launching batch jobs in run_C-ESM-EP.py
+        # This could be a location shared among users.
+        # This can also be a location visible only from compute cluster (aneto)
+        climaf_cache = '/data/scratch/globc/dcom/CMIP6_TOOLS/C-ESM-EP/CESMEP_climaf_cache'
+        
+        # Root on file system for atlas.
+        # If using http for rendering, this should be somehow visible by the http server
+        # This value is used as 'pathwebspace' in run_C-ESM-EP.py
+        # It will be complemented with 'C-ESM-EP/'
+        # This location can be shared among users, as username will be added at end of data path
+        path_to_cesmep_output_rootdir = '/data/scratch/globc/dcom/CMIP6_TOOLS/C-ESM-EP/CESMEP_html'
+        
+        # Describe the rendering method; if it is http, must match the workspace and the adress for http server
+        # For now we do not use http:// but file:// for atlas rendering
+        root_url_to_cesmep_outputs = 'http://cerfacs.fr/giec6/C-ESM-EP/CESMEP_html/'
+        #
+        path_to_cesmep_output_rootdir_on_web_server = None
 
-    # Root on file system for atlas.
-    # If using http for rendering, this should be somehow visible by the http server
-    # This value is used as 'pathwebspace' in run_C-ESM-EP.py
-    # It will be complemented with 'C-ESM-EP/'
-    # This location can be shared among users, as username will be added at end of data path
-    path_to_cesmep_output_rootdir = '/scratch/globc/' + username + '/C-ESM-EP/CESMEP_html'
-
-    # Describe the rendering method; if it is http, must match the workspace and the adress for http server
-    # For now we do not use http:// but file:// for atlas rendering
-    root_url_to_cesmep_outputs = 'file://'+path_to_cesmep_output_rootdir
-    #
-    path_to_cesmep_output_rootdir_on_web_server = None
-
-
-# -- At Cerfacs
-# On scylla
-if os.path.exists('/data/scratch/globc'):
-    # Climaf Cache location - used for launching batch jobs in run_C-ESM-EP.py
-    # This could be a location shared among users.
-    # This can also be a location visible only from compute cluster (aneto)
-    climaf_cache = '/data/scratch/globc/dcom/CMIP6_TOOLS/C-ESM-EP/CESMEP_climaf_cache'
-
-    # Root on file system for atlas.
-    # If using http for rendering, this should be somehow visible by the http server
-    # This value is used as 'pathwebspace' in run_C-ESM-EP.py
-    # It will be complemented with 'C-ESM-EP/'
-    # This location can be shared among users, as username will be added at end of data path
-    path_to_cesmep_output_rootdir = '/data/scratch/globc/dcom/CMIP6_TOOLS/C-ESM-EP/CESMEP_html'
-
-    # Describe the rendering method; if it is http, must match the workspace and the adress for http server
-    # For now we do not use http:// but file:// for atlas rendering
-    root_url_to_cesmep_outputs = 'http://cerfacs.fr/giec6/C-ESM-EP/CESMEP_html/'
-    #
-    path_to_cesmep_output_rootdir_on_web_server = None
+    elif os.path.exists('/scratch/globc'):
+        # Climaf Cache location - used for launching batch jobs in run_C-ESM-EP.py
+        # This could be a location shared among users.
+        # This can also be a location visible only from compute cluster (aneto)
+        climaf_cache = '/scratch/globc/' + username + '/C-ESM-EP/CESMEP_climaf_cache'
+        
+        # Root on file system for atlas.
+        # If using http for rendering, this should be somehow visible by the http server
+        # This value is used as 'pathwebspace' in run_C-ESM-EP.py
+        # It will be complemented with 'C-ESM-EP/'
+        # This location can be shared among users, as username will be added at end of data path
+        path_to_cesmep_output_rootdir = '/scratch/globc/' + username + '/C-ESM-EP/CESMEP_html'
+        
+        # Describe the rendering method; if it is http, must match the workspace and the adress for http server
+        # For now we do not use http:// but file:// for atlas rendering
+        root_url_to_cesmep_outputs = 'file://'+path_to_cesmep_output_rootdir
+        #
+        path_to_cesmep_output_rootdir_on_web_server = None
 
 
