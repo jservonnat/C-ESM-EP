5dcc99f070109a525d03e221a4d4a5eb28f40baf
Antoine Bierjon <abierjon@ipsl.fr>
2025-04-07 18:49:15 +0200

    1st commit for FWcycle comparison

---
+++ dev_comparisons/FWcycle_comparison/FWcycle/params_FWcycle.py
@@ -0,0 +1,32 @@
+# ------------------------------------------------------------------------------------------ \
+# --                                                                                        - \
+# --                                                                                         - \
+# --      User Interface for:                                                                 - \
+# --                                                                                           - \
+# --          CliMAF Earth System Model Evaluation Platform                                     - \
+# --             - component: AtlasExplorer                                                 - |
+# --                                                                                             - |
+# --      Developed within the ANR Convergence Project                                           - |
+# --      CNRM GAME, IPSL, CERFACS                                                               - |
+# --      Contributions from CNRM, LMD, LSCE, NEMO Group, ORCHIDEE team.                         - |
+# --      Based on CliMAF: WP5 ANR Convergence, S. Senesi (CNRM) and J. Servonnat (LSCE - IPSL)  - |
+# --                                                                                             - |
+# --      J. Servonnat, S. Senesi, L. Vignon, MP. Moine, O. Marti, E. Sanchez, F. Hourdin,       - |
+# --      I. Musat, M. Chevallier, J. Mignot, M. Van Coppenolle, J. Deshayes, R. Msadek,         - |
+# --      P. Peylin, N. Vuichard, J. Ghattas, F. Maignan, A. Ducharne, P. Cadule,                - |
+# --      P. Brockmann, C. Rousset                                                               - |
+# --                                                                                             - |
+# --      Contact: jerome.servonnat@lsce.ipsl.fr                                                 - |
+# --                                                                                             - |
+# --                                                                                            - /
+# --                                                                                           - /
+# --------------------------------------------------------------------------------------------- /
+
+
+# -- Empty params_${component}.py file! everything is done from diagnostics_${component}.py
+
+
+# ---------------------------------------------------------------------------------------- #
+# -- END                                                                                -- #
+# ---------------------------------------------------------------------------------------- #
+
